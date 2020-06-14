a87138f11bd56dbd7a3b7522a595f3ccb7049e87
Dmytro Buryak <dmytro.buryak@gmail.com>
2020-06-15 02:35:11 +0300

    Configured logging

--- src/main/java/com/dburyak/mybooks/perf/micronaut/async/Application.java
+++ src/main/java/com/dburyak/mybooks/perf/micronaut/async/Application.java
@@ -1,18 +1,21 @@
-package mybooks.perf.micronaut.async;
+package com.dburyak.mybooks.perf.micronaut.async;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
+import lombok.extern.log4j.Log4j2;
 
 @OpenAPIDefinition(
     info = @Info(
             title = "mybooks-perf-micronaut-async",
-            version = "0.0"
+            version = "0.1"
     )
 )
+@Log4j2
 public class Application {
 
     public static void main(String[] args) {
+        log.info("starting application");
         Micronaut.run(Application.class);
     }
 }
